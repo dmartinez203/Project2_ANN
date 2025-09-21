56185a64b6362b82de374de654138084f0d6f8af
dmartinez203 <122460783+dmartinez203@users.noreply.github.com>
2025-09-21 16:01:41 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ scaler, label_names, model = load_resources()
 # App interface
 # --------------------------
 st.title("🏀 NBA Optimal Team Selector (MLP)")
-st.caption("Select a 5-year window, predict player performance, and build an optimal team.")
+st.caption("Select a 5-year window, predict player performance, and build an optimal team!")
 
 uploaded = st.file_uploader("Upload NBA CSV", type="csv")
 
